2d792eed6e75b2ee2c38795ae19d9956b2014a15
Juan Pulido <alephart@gmail.com>
2021-02-04 12:59:29 -0500

    new config Jest +  TestingLibrary + Nextjs

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,12 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
   moduleNameMapper: {
+    '^@/components/(.*)': '<rootDir>/src/components/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   } 
 };
 
+// fix path jest with nextjs
+// https://github.com/kulshekhar/ts-jest/issues/269#issuecomment-318163416
+// I use: https://nextjs.org/docs/advanced-features/module-path-aliases
