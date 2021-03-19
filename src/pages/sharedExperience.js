7fd7e13b0f94adcab7f8308d8376e50aeae48039
Juan Pulido <alephart@gmail.com>
2021-03-19 18:01:36 -0500

    Adjust pages not required

--- src/pages/sharedExperience.js
+++ src/pages/sharedExperience.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const SharedExperience = () => {
-    Return (
-        <div className="sharedExperience">
-        </div>
+    return (
+        <div className="sharedExperience"></div>
     )
 }
     
