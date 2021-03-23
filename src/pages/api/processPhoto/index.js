b7228aa4a1e96dfcf29014877903725ec3077ef6
Juan Pulido <alephart@gmail.com>
2021-03-23 14:00:23 -0500

    Fix: watermark

--- src/pages/api/processPhoto/index.js
+++ src/pages/api/processPhoto/index.js
@@ -169,7 +169,7 @@ export default async (req, res) => {
     };
 
     // remove posible video exists 
-    removeFileSync(dataFinal.output);
+    //removeFileSync(dataFinal.output);
 
     await placeWatermarkOnVideo(dataFinal);
 
