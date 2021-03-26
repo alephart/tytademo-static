82fe73ecf870837cee9a437e50f76c3621ae8700
Juan Pulido <alephart@gmail.com>
2021-03-26 07:02:45 -0500

    ...

--- src/pages/api/processPhoto/index.js
+++ src/pages/api/processPhoto/index.js
@@ -37,8 +37,8 @@ export default async (req, res) => {
     
     // 1. Get photo conver to binary and: upload to S3, upload to reface API
     await writeFile(pathFinalPhoto, imageBuffer.data);
-    const binaryFile = loadFileSync(pathFinalPhoto);
     const photoLocation = uploadFile(pathFinalPhoto, nameFilePhoto, 'image', true);
+    const binaryFile = loadFileSync(pathFinalPhoto);
     const uploadReface = await uploadAsset(binaryFile, `image/${imageBuffer.ext}`);
 
     responseReface = JSON.parse(uploadReface);
