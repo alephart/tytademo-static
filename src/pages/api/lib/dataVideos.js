f84dc0232887fc855776d591107dd9e346dc965d
Juan Pulido <alephart@gmail.com>
2021-04-14 18:35:23 -0500

    adjust only data

--- src/pages/api/lib/dataVideos.js
+++ src/pages/api/lib/dataVideos.js
@@ -1,7 +1,63 @@
-// Here compose all videos that were previously adjusted in Reface. 
+
+// Here compose all videos that were previously adjusted in Reface.
+
+// The functions return array with objects. 
+// Each object is how it should be sent to Reface to do the swap.
+
+// Step 3. Swap videos [sidekick] and get ids
+export const videosListMan = (faceId) => {
+  if (!faceId) return null;
+  return [
+    { // 02
+      intensity: 1,
+      video_id: '85434d30-bdd3-48b4-bcde-4f0681e57ed6',
+      facemapping: {
+        '4f06dbd1-0568-422d-bbd2-9d968068bf73': [
+          `${faceId}`
+        ]
+      },
+    },
+    { // 06
+      intensity: 1,
+      video_id: 'e939a4f8-0246-48e6-8ef0-4a7884a859e1',
+      facemapping: {
+        '9a78b1f9-53d6-4d0e-a59f-6473e9cada92': [
+          `${faceId}`
+        ]
+      },
+    },
+    { // 08
+      intensity: 1,
+      video_id: '4826c84b-b1a9-4ac1-b953-b00cfb9e725b',
+      facemapping: {
+        '521ce870-36cf-46d5-bffe-68429eaa4d05': [
+          `${faceId}`
+        ]
+      },
+    },
+    { // 12
+      intensity: 1,
+      video_id: '39425615-2d94-49ab-87ea-49c357e20f34',
+      facemapping: {
+        'dc431f74-7fb7-4cd2-94db-8908f3371184': [
+          `${faceId}`
+        ]
+      },
+    },
+    { // 14
+      intensity: 1,
+      video_id: '8ffab2cf-dc6f-419d-bb0f-3acae4960f2d',
+      facemapping: {
+        '05a88bb2-b51f-4cac-9bb5-c52a79af5d75': [
+          `${faceId}`
+        ]
+      },
+    },
+  ];
+};
 
 // 3. Swap videos [man] and get ids
-export const videosListMan = (faceId) => {
+export const videosListManNO = (faceId) => {
   if (!faceId) return null;
   return [
     { // 02
@@ -77,20 +133,26 @@ export const videosListWoman = (faceId) => {
   ];
 };
 
+// array with all video clips to use (store into /temp/footage)
 export const videoListAll = [
-  { name: '01-NoSwap_tbn.mp4', character: '' },
-  { name: '02-SwapSidekick_tbn.mp4', character: 'man' },
-  { name: '03-NoSwap_tbn.mp4', character: '' },
-  { name: '04-SwapWoman_tbn.mp4', character: 'woman' },
-  { name: '05-NoSwap_tbn.mp4', character: '' },
-  { name: '06-SwapSidekick_tbn.mp4', character: 'man' },
-  { name: '07-NoSwap_tbn.mp4', character: '' },
-  { name: '08-SwapWoman_tbn.mp4', character: 'woman' },
-  { name: '09-NoSwap_tbn.mp4', character: '' },
-  { name: '10-SwapSidekick_tbn.mp4', character: 'man' },
-  { name: '11-NoSwap_tbn.mp4', character: '' },
-  { name: '12-SwapWoman_tbn.mp4', character: 'woman' },
-  { name: '13-NoSwap_tbn.mp4', character: '' },
-  { name: '14-SwapWoman_tbn.mp4', character: 'woman' },
-  { name: '15-NoSwap_tbn.mp4', character: '' },
+  { name: 'footage/001.mp4', character: '' },
+  { name: 'footage/002-SIDEKICK.mp4', character: 'man' },
+  { name: 'footage/003.mp4', character: '' },
+  { name: 'footage/004-WOMAN.mp4', character: 'woman' },
+  { name: 'footage/005.mp4', character: '' },
+  { name: 'footage/006-SIDEKICK.mp4', character: 'man' },
+  { name: 'footage/007.mp4', character: '' },
+  { name: 'footage/008-SIDEKICK.mp4', character: 'man' },
+  { name: 'footage/009.mp4', character: '' },
+  { name: 'footage/010-WOMAN.mp4', character: 'woman' },
+  { name: 'footage/011.mp4', character: '' },
+  { name: 'footage/012-SIDEKICK.mp4', character: 'man' },
+  { name: 'footage/013.mp4', character: '' },
+  { name: 'footage/014-WOMAN-SIDEKICK.mp4', character: 'man' },
+  { name: 'footage/015.mp4', character: '' },
+  { name: 'footage/016-WOMAN.mp4', character: 'woman' },
+  { name: 'footage/017.mp4', character: '' },
+  { name: 'footage/018-WOMAN.mp4', character: 'woman' },
+  { name: 'footage/019.mp4', character: '' },
+  { name: 'footage/020-WOMAN.mp4', character: 'woman' },
 ];
