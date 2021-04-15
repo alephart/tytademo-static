67dcc20b683109d38d0ab50dad57dc1bd6c5de28
Juan Pulido <alephart@gmail.com>
2021-04-14 23:20:35 -0500

    update data woman

--- src/pages/api/lib/dataVideos.js
+++ src/pages/api/lib/dataVideos.js
@@ -56,79 +56,63 @@ export const videosListMan = (faceId) => {
   ];
 };
 
-// 3. Swap videos [man] and get ids
-export const videosListManNO = (faceId) => {
-  if (!faceId) return null;
-  return [
-    { // 02
-      intensity: 1,
-      video_id: '8b63e515-9762-4a26-85d7-f21762996e11',
-      facemapping: {
-        'b41cf3f3-df0a-4223-a4fd-fbcd2bb763cc': [
-          `${faceId}`
-        ]
-      }
-    },
-    { // 06
-      intensity: 1,
-      video_id: '2b5e1d30-0ff3-4efe-8880-b805e36e0615',
-      facemapping: {
-        '02ad0e8a-f6da-45dc-956f-42a617c7ccaa': [
-          `${faceId}`
-        ]
-      }
-    },
-    { // 10
-      intensity: 1,
-      video_id: '00a4c0a6-e9d4-4341-b646-d173fc7119d3',
-      facemapping: {
-        '116e2ffc-9441-4157-bac4-b51c931d23d4': [
-          `${faceId}`
-        ]
-      }
-    },
-  ];
-};
-
-// 3. Swap videos [woman] and get ids
+// Step 3. Swap videos [woman] and get ids
 export const videosListWoman = (faceId) => {
   if (!faceId) return null;
   return [
     { // 04
       intensity: 1,
-      video_id: '0aceca88-6ddd-4069-a61e-ed5db7200ebc',
+      video_id: 'a8237e9b-9c0c-4722-9e6c-b9a8b9a5c411',
       facemapping: {
-        '50b5f476-d5fe-48e3-be40-47665f09b4fc': [
+        '0736c057-c6b9-445d-84a4-c2708bc8d02f': [
           `${faceId}`
         ]
-      }
+      },
     },
-    { // 08
+    { // 10
       intensity: 1,
-      video_id: '05311379-2361-4518-b89f-29c9af531f95',
+      video_id: '3e3f47d7-25f0-4da9-95de-c85605a92190',
       facemapping: {
-        '635db886-1ddd-4899-828a-6f40e9f3da80': [
+        '11983f1c-d97a-42b7-a465-b315a1a5c239': [
           `${faceId}`
         ]
-      }
-    },
-    { // 12
-      intensity: 1,
-      video_id: 'f7624d43-d092-48ec-8b09-ba8f6aee703d',
-      facemapping: {
-        '22d07bf7-84d9-461b-a1f2-09e02693c0c8': [
-          `${faceId}`
-        ]
-      }
+      },
     },
     { // 14
       intensity: 1,
-      video_id: '0043c228-14be-4075-b11a-d9e5cbe66ed2',
+      video_id: '8ffab2cf-dc6f-419d-bb0f-3acae4960f2d',
       facemapping: {
-        '5310ae0a-c6a7-4c10-9993-e6390fc5405c': [
+        '8cc715fc-cfaa-4737-81da-4db3f4bd6b80': [
           `${faceId}`
         ]
-      }
+      },
+    },
+    { // 16
+      intensity: 1,
+      video_id: '53b8b899-0f4b-4011-b3ab-27d775e209d3',
+      facemapping: {
+        'dfff0df7-8dab-40bf-bb8b-54587e4ada64': [
+          `${faceId}`
+        ]
+      },
+    },
+    { // 18
+      intensity: 1,
+      video_id: '76a75c2b-570c-440e-8685-69996dd395ef',
+      facemapping: {
+        '5b525dd1-1add-4534-8713-bacb82d955ef': [
+          `${faceId}`
+        ]
+      },
+    },
+    { // 20
+      intensity: 1,
+      video_id: '5efc1653-8eee-455a-8b8f-8fc784d236a8',
+      facemapping: {
+        'b43cf87f-4f1f-4d66-9874-a60ba5d32b5f': [
+          `${faceId}`
+        ]
+     , },
     },
   ];
 };
@@ -148,7 +132,7 @@ export const videoListAll = [
   { name: 'footage/011.mp4', character: '' },
   { name: 'footage/012-SIDEKICK.mp4', character: 'man' },
   { name: 'footage/013.mp4', character: '' },
-  { name: 'footage/014-WOMAN-SIDEKICK.mp4', character: 'man' },
+  { name: 'footage/014-WOMAN-SIDEKICK.mp4', character: 'woman' },
   { name: 'footage/015.mp4', character: '' },
   { name: 'footage/016-WOMAN.mp4', character: 'woman' },
   { name: 'footage/017.mp4', character: '' },
