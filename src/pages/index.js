f1ca744ec9d8108799e39aff891b98cbc304e119
Juan Pulido <alephart@gmail.com>
2021-04-15 18:53:13 -0500

    feat: auth password session

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
+import { signIn, signOut, useSession } from "next-auth/client"
+import Link from "next/link"
 import styles from '../styles/Home.module.scss'
 import TakePhoto from '@/components/TakePhoto'
 
+
 export default function Home() {
+  const [session, loading] = useSession();
   const [facingMode, setFacingMode] = useState('user');
-  const [cameraOn, setCameraOn] = useState(true);
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
 
   return (
     <div className={styles.container}>
@@ -14,16 +21,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <header>
         <h1>Toyota DeepFake</h1>
+        {session && (
+          <>
+            Signed in as {session.user.name}  <button onClick={signOut}>Sign out</button>
+          </>
+        )}
+        {!session && (
+          <>
+            <button onClick={signIn}>Sign in Toyota Experience DEMO</button>
+          </>
+        )}
+      </header>
 
-        <TakePhoto facingMode={facingMode} setFacingMode={setFacingMode} /> 
+      <main className={styles.main}>
+        {session && (
+          <>
+            <TakePhoto facingMode={facingMode} setFacingMode={setFacingMode} />
+          </>
+        )}
+
       </main>
-
       <footer className={styles.footer}>
         Copyright Toyota
       </footer>
-
     </div>
   )
 }
