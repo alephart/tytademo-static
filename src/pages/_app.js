4366d1a8a132c222682697af183742076e06f86f
Juan Pulido <alephart@gmail.com>
2021-04-19 14:58:30 -0500

    Merge TOYOT-40, adjust and new version

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
-import '../styles/globals.css'
-import '../styles/general.scss'
+import '@/styles/globals.css'
+import '@/styles/general.scss'
 
 import { Provider } from "next-auth/client"
 
-function App({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
