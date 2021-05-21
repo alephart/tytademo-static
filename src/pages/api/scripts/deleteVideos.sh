89d0329a6058bd242f22bf1706107b7d4be15e9e
Juan Pulido <alephart@gmail.com>
2021-05-20 23:47:30 -0500

    adjust file and path

--- src/pages/api/scripts/deleteVideos.sh
+++ src/pages/api/scripts/deleteVideos.sh
@@ -1,2 +1,5 @@
-## this file content a command the removes all mp4 files from /temp
-rm /Users/codemds/mds/toyota/tytademo/temp/*.mp4
+#!/bin/bash
+# this file content a command the removes all mp4 files from /temp
+
+rm ../../../../temp/*.mp4
+echo "files remove!"
