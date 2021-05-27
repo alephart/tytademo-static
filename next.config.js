ae81c21099130e906ef807a19f24d607431d4e75
Juan Pulido <alephart@gmail.com>
2021-05-27 11:01:05 -0500

    config language detection

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  i18n: {
+    // These are all the locales to support in application
+    locales: ['en', 'es'],
+    // This is the default locale to be used when visiting
+    // a non-locale prefixed path e.g. `/experience`
+    defaultLocale: 'en',
+  },
+}
