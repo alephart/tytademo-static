fa65cca9e1719ca915cf333e397478e3d10b4111
Juan Pulido <alephart@gmail.com>
2021-05-28 08:25:13 -0500

    no available

--- src/pages/not-available.js
+++ src/pages/not-available.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-
 const NotAvailable = () => {
-    return (
-        <div  className="notAvailable">
-            <div className="boxItemsNotAvailable">
-                <div className="copyNotAvailable">
-                    <img className="logoToyota" src="/images/logo-toyota.png" alt=""/>
-                    <h2>
-                        esta campaña no está disponible en tu país.
-                    </h2>
-                    <p>
-                        Conoce el nuevo Corolla Apex: {' '}
-                        <a href="https://www.toyota.com/corolla/">
-                            www.toyota.com/corolla
-                        </a>
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className='notAvailable'>
+      <div className='boxItemsNotAvailable'>
+        <div className='copyNotAvailable'>
+          <img className='logoToyota' src='/images/logo-toyota.png' alt='' />
+          <h2>Esta campaña no está disponible en tu país.</h2>
+          <p>
+            Conoce el nuevo Corolla Apex:{' '}
+            <a href='https://www.toyota.com/corolla/'>www.toyota.com/corolla</a>
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default NotAvailable;
