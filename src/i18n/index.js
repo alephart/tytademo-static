ca35dc692f359572ec976bfae9df7cdf655781f8
Juan Pulido <alephart@gmail.com>
2021-06-01 19:22:06 -0500

    Default language english

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -16,8 +16,8 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
-        lng: 'es',
-        fallbackLng: 'es',
+        lng: 'en',
+        fallbackLng: 'en',
         keySeparator: false,
         interpolation: {
             escapeValue: false,
