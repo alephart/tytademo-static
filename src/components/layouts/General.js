097e29330bef7b1040a79304948fa5ee63920990
jose Kromeon <jose@kromeon.com>
2021-06-04 11:20:10 -0500

    delete landscape

--- src/components/layouts/General.js
+++ src/components/layouts/General.js
@@ -5,6 +5,18 @@ const Layout = ({ children }) => {
     <div className={`container ${isAndroid ? 'android' : ''}`}>
       {children}
       {/* aqui va version desktop */}
+      <div className='portrait'>
+          <div className="notAvailable">
+              <div className="boxItemsNotAvailable">
+                  <div className="copyNotAvailable">
+                      <img className="logoToyota" src="/images/logo-toyota.png" alt=""/>
+                      <h2>
+                          gira tu celular para seguir con la experiencia
+                      </h2>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
   )
 };
