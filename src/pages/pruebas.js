95d751b65a94afba7062704f0683e8a322f6bd4f
Juan Pulido <alephart@gmail.com>
2021-06-04 07:49:52 -0500

    pruebas

--- src/pages/pruebas.js
+++ src/pages/pruebas.js
@@ -1,11 +1,20 @@
-import { Loading } from '@/components/Anims';
+import { useState } from 'react';
 import Layout from '@/components/layouts/General';
+import { Help } from '@/components/DialogsTyta';
+import { Loading } from '@/components/Anims';
+import { Button } from '@material-ui/core/';
 
 const Pruebas = () => {
+  const [help, setHelp] = useState(false);
+
   return(
     <Layout>
       <p>Loading</p>
       <Loading />
+
+      <Button onClick={() => setHelp(!help)}>Active Modal!</Button>
+
+      <Help isOpen={help} setIsOpen={setHelp} />
 
     </Layout>
   )
