3cdf050e509c5e9667999cf640190af4aceff959
jose Kromeon <jose@kromeon.com>
2021-06-04 18:10:20 -0500

    fix estilos landscape y  desktop

--- src/components/layouts/StartPage.js
+++ src/components/layouts/StartPage.js
@@ -1,6 +1,8 @@
 import Meta from './Meta';
+import { useTranslation } from 'next-i18next';
 
 const Layout = ({ children, pageTitle, description, ...props }) => {
+  const { t } = useTranslation('common');
   const meta = {
     pageTitle: 'Featuring You Lunay X Toyota',
     description: '',
@@ -15,6 +17,21 @@ const Layout = ({ children, pageTitle, description, ...props }) => {
       <Meta meta={meta} />
       <div className="startPage">
         {children}
+        <div className='landscape'>
+            <div className="notAvailable">
+                <div className="boxItemsNotAvailable">
+                    <div className="copyNotAvailable">
+                        <img className="logoToyota" src="/images/logo-toyota.png" alt=""/>
+                        <h2 className="desktop">
+                          {t("desktop")}
+                        </h2>
+                        <h2 className="mobile">
+                          {t("landscape")}
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     </>
   )
