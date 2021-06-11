af156943550f49a3759d7d356ea19ede15641c62
Juan Pulido <alephart@gmail.com>
2021-06-11 05:44:45 -0500

    Email and template

--- src/pages/api/lib/sendEmail.js
+++ src/pages/api/lib/sendEmail.js
@@ -1,39 +1,69 @@
+const path = require('path');
 const nodemailer = require('nodemailer');
 const Email = require('email-templates');
 
-// message example
-message = {
-  from: "jpulido@mdsdigital.com",
-  to: "jpulido@mdsdigital.com",
-  subject: "Test email",
-  text: "Hello SMTP Email",
-  html: "<h1>Hello SMTP Email</h1>"
+const pathToTemplates = path.join(__dirname, '../email/templates');
+
+const sendEmail = async (options) => {
+  const { firstname, lastname, email, urlVideo } = options;
+
+  let transporter = nodemailer.createTransport({
+    // host: "email-smtp.us-east-1.amazonaws.com",
+    // port: 465,
+    // auth: {
+    //   user: "AKIASED4EMDDXHRVVACV",
+    //   pass: "BO0EZTk2fdCZTA54suzroNKGjFpMAUozwmUtgWjzb3sv"
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "223d99de78ce34",
+      pass: "d8b1baa9c9570d"
+    }
+  });
+  
+  const email = new Email({
+    transport: transporter,
+    send: true,
+    preview: false,
+    views: {
+      root: pathToTemplates,
+    },
+  });
+  
+  await email.send({
+    template: 'register',
+    message: {
+      from: 'jpulido@mdsdigital.com',
+      to: 'juancpulidos@gmail.com',
+    },
+    locals: {
+      firstname,
+      lastname,
+      email,
+      urlVideo,
+
+    }
+  }).then(() => console.log('email has been send!'));
+  
+}
+
+module.exports = {
+  sendEmail,
 };
 
-let transporter = nodemailer.createTransport({
-  host: "email-smtp.us-east-1.amazonaws.com",
-  port: 465,
-  auth: {
-    user: "AKIASED4EMDDXHRVVACV",
-    pass: "BO0EZTk2fdCZTA54suzroNKGjFpMAUozwmUtgWjzb3sv"
-  // host: "smtp.mailtrap.io",
-  // port: 2525,
-  // auth: {
-  //   user: "223d99de78ce34",
-  //   pass: "d8b1baa9c9570d"
-  }
-});
+// // message example
+// mailOptions = {
+//   from: "jpulido@mdsdigital.com",
+//   to: "jpulido@mdsdigital.com",
+//   subject: "Test email",
+//   text: "Hello SMTP Email",
+//   html: "<h1>Hello SMTP Email</h1>"
+// };
 
-const email = new Email({
-  transport: transporter,
-  send: true,
-  preview: false,
-});
-
-transporter.sendMail(message, function(err, info) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(info);
-  }
-});
+// transporter.sendMail(mailOptions, function(err, info) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(info);
+//   }
+// });
