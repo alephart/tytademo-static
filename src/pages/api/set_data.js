8dc2eb6eefead10232b58278e71e98822cac0262
Juan Pulido <alephart@gmail.com>
2021-06-17 13:05:36 -0500

    Header error req and email message adjust

--- src/pages/api/set_data.js
+++ src/pages/api/set_data.js
@@ -26,6 +26,8 @@ export default async (req, res) => {
     footage,
   } = req.body;
 
+  let userExist = false;
+
   try {
     // first check unique email address  
     await fetch(`${configAdmin.url_api}/checkEmail`, {
@@ -34,95 +36,95 @@ export default async (req, res) => {
       headers: { 'Content-Type': 'application/json' },
     })
     .then(res => res.json())
-    .then(json => {
-      if(json.exist) {
-        // return
-        res.status(200).send({ success: false, action: 'checkEmail', userExist: json.exist });
-      }
-    })
+    .then(json => userExist = json.exist)
     .catch(error => {
       console.error(error);
       res.status(500).send({ success: false, error});
     });
 
-    // send data to API Toyota Admin
-    const dataAdmin = {
-      participant_id: userId,
-      firstname,
-      lastname,
-      email,
-      zipcode,
-      character,
-      url_photo: urlPhoto,
-      url_video: urlVideo,
-      productnews: productNews,
-      testdrive: testDrive,
-      footage,
-    };
-
-    //console.log(dataAdmin);
-
-    let jsonAdmin;
-
-    try {
-      const resAdmin = await fetch(`${configAdmin.url_api}/participant`, {
-        method: 'POST',
-        body:    JSON.stringify(dataAdmin),
-        headers: { 'Content-Type': 'application/json' },
-      });
+    if(userExist) {
+      res.status(200).send({ success: false, action: 'checkEmail', userExist });
     
-      jsonAdmin = await resAdmin.json();
+    } else {
+      // send data to API Toyota Admin
+      const dataAdmin = {
+        participant_id: userId,
+        firstname,
+        lastname,
+        email,
+        zipcode,
+        character,
+        url_photo: urlPhoto,
+        url_video: urlVideo,
+        productnews: productNews,
+        testdrive: testDrive,
+        footage,
+      };
+
+      //console.log(dataAdmin);
+
+      let jsonAdmin;
+
+      try {
+        const resAdmin = await fetch(`${configAdmin.url_api}/participant`, {
+          method: 'POST',
+          body:    JSON.stringify(dataAdmin),
+          headers: { 'Content-Type': 'application/json' },
+        });
       
-      if(!jsonAdmin.participant) {
-        // return
-        res.status(200).send({ success: false, action: 'saveParticipant', userExist: jsonAdmin.participant });
+        jsonAdmin = await resAdmin.json();
+        
+        if(!jsonAdmin.participant) {
+          // return
+          res.status(200).send({ success: false, action: 'saveParticipant', userExist: jsonAdmin.participant });
+        }
+
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, error});
       }
 
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ success: false, error});
-    }
+      // send data To API MoZeus
+      // const dataMozeus = {
+      //   firstName: firstname,
+      //   lastName: lastname,
+      //   email,
+      //   zipCode: zipcode,
+      // }
 
-    // send data To API MoZeus
-    // const dataMozeus = {
-    //   firstName: firstname,
-    //   lastName: lastname,
-    //   email,
-    //   zipCode: zipcode,
-    // }
+      // const resMozeus = await fetch(`${configMozeus.url_api}/${configMozeus.user_id}`, {
+      //   method: 'POST',
+      //   body:    JSON.stringify(dataMozeus),
+      //   headers: { 'Content-Type': 'application/json' },
+      // });
 
-    // const resMozeus = await fetch(`${configMozeus.url_api}/${configMozeus.user_id}`, {
-    //   method: 'POST',
-    //   body:    JSON.stringify(dataMozeus),
-    //   headers: { 'Content-Type': 'application/json' },
-    // });
+      // const jsonMozeus = await resMozeus.json();
+      // console.log(jsonMozeus);  
 
-    // const jsonMozeus = await resMozeus.json();
-    // console.log(jsonMozeus);  
+      // email sending
+      const config = {
+        host: process.env.AWS_SES_HOST,
+        port: process.env.AWS_SES_PORT,
+        user: process.env.AWS_SES_USERNAME,
+        pass: process.env.AWS_SES_PASSWORD,
+        from: process.env.NEXT_PUBLIC_FROM_EMAIL,
+      };
 
-    // email sending
-    const config = {
-      host: process.env.AWS_SES_HOST,
-      port: process.env.AWS_SES_PORT,
-      user: process.env.AWS_SES_USERNAME,
-      pass: process.env.AWS_SES_PASSWORD,
-      from: process.env.NEXT_PUBLIC_FROM_EMAIL,
-    };
+      const options = {
+        firstname,
+        lastname,
+        email,
+        urlShare,
+        locale,
+      };
 
-    const options = {
-      firstname,
-      lastname,
-      email,
-      urlShare,
-      locale,
-    };
+      //console.log(config, options)
 
-    //console.log(config, options)
-
-    await sendEmail(config, options);
-    
-    // return
-    await res.status(200).send({ success: true, dataBody: req.body });
+      await sendEmail(config, options);
+      
+      // return
+      await res.status(200).send({ success: true, dataBody: req.body });
+    } // eof userExist
 
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
