511f17ba3588226e865aa89389662b3901f762d5
jose Kromeon <jose@kromeon.com>
2021-06-18 13:14:55 -0500

    fix videos and news animations

--- src/components/layouts/StartPage.js
+++ src/components/layouts/StartPage.js
@@ -1,30 +1,33 @@
 import Meta from './Meta';
 import { useTranslation } from 'next-i18next';
+import { AnimatePresence } from "framer-motion";
 
 const Layout = ({ children, ...props }) => {
   console.log('Layout Home', props)
   const { t } = useTranslation('common');
   return (
     <>
-      <Meta {...props} />
-      <div className="startPage">
-        {children}
-        <div className='landscape'>
-            <div className="notAvailable">
-                <div className="boxItemsNotAvailable">
-                    <div className="copyNotAvailable">
-                        <img className="logoToyota" src="/images/logo-toyota.png" alt=""/>
-                        <h2 className="desktop">
-                          {t("desktop")}
-                        </h2>
-                        <h2 className="mobile">
-                          {t("landscape")}
-                        </h2>
-                    </div>
-                </div>
-            </div>
+      <AnimatePresence exitBeforeEnter>
+        <Meta {...props} />
+        <div className="startPage">
+          {children}
+          <div className='landscape'>
+              <div className="notAvailable">
+                  <div className="boxItemsNotAvailable">
+                      <div className="copyNotAvailable">
+                          <img className="logoToyota" src="/images/logo-toyota.png" alt=""/>
+                          <h2 className="desktop">
+                            {t("desktop")}
+                          </h2>
+                          <h2 className="mobile">
+                            {t("landscape")}
+                          </h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
         </div>
-      </div>
+      </AnimatePresence>
     </>
   )
 };
