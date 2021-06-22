a91fb716d0fe3d2634e3c9585cab64abad2af07f
Juan Pulido <alephart@gmail.com>
2021-06-22 08:48:56 -0500

    Add GTM with component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 import '@/styles/general.scss'
 
+import { useEffect } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import { Provider as SessionProvider } from "next-auth/client";
+import TagManager from 'react-gtm-module';
 
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5H22NDS' });
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
