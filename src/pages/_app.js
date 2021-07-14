169a6ac8bbb6703ac00ccd45a0e93bca966b895e
Juan Pulido <alephart@gmail.com>
2021-07-14 13:05:09 -0500

    fix xmeta tags with next - rollback nextjs 11 - rollback material UI - disabled  react-gtm-module - Add GTM classic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,12 +4,12 @@ import '@/styles/general.scss'
 import { useEffect } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import { Provider as SessionProvider } from "next-auth/client";
-import TagManager from 'react-gtm-module';
+//import TagManager from 'react-gtm-module';
 
 const MyApp = ({ Component, pageProps }) => {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-5H22NDS' });
-  }, []);
+  // useEffect(() => {
+  //   TagManager.initialize({ gtmId: 'GTM-5H22NDS' });
+  // }, []);
 
   return (
     <SessionProvider session={pageProps.session}>
