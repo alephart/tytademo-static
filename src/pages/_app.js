8b8cb5ebbc9caa3872fa4ac5af47ca04d2dc09c4
Juan Pulido <alephart@gmail.com>
2021-07-26 10:56:55 -0500

    Return to react gtm module. Is another error with GTM.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import '@/styles/globals.css'
 import '@/styles/general.scss'
 
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
 const MyApp = ({ Component, pageProps }) => {
-  return (
-  <Component {...pageProps} />
-  )
-};
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5H22NDS' });
+  }, []);
+
+  return <Component {...pageProps} />
+}
 
 export default MyApp;
