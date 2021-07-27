3f4a41121aa0e24b4e92b90787f1f7fc2389cdbf
fernandoTorresKromeon <fernandojoset@gmail.com>
2021-07-27 17:54:32 -0500

    add timeout en node js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // server.js
 const next = require("next");
+const timeout = require("connect-timeout");
 
 // Get environment
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
+
+app.use(timeout("60s"));
 
 // Return app
 module.exports = {
