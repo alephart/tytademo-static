4de0e1e3b6418770a3958e06a50c92f0ac1b6b5f
Juan Pulido <alephart@gmail.com>
2021-07-27 13:46:07 -0500

    endpoint api to test measuring jMeter

--- src/pages/api/test-measuring.js
+++ src/pages/api/test-measuring.js
@@ -1,27 +1,66 @@
 const path = require('path');
-const { uploadFile } = require('./lib/bucketS3API');
+const cuid = require('cuid');
 const { concatVideosTxtFluent, changeTrackFluent } = require('./lib/ffmpegActions');
-const { writeFileSync } = require('./lib/fileActions');
-const { dataSwapVideos, downloadSwapVideos, buildFileVideos, adjustTbnVideos } = require('./lib/refaceActions');
+const { writeFileSync, loadFileSync, removeFileSync } = require('./lib/fileActions');
+const { buildFileVideos, adjustTbnVideos } = require('./lib/refaceActions');
+const { videoListAll } = require('./lib/dataVideos');
+
+const fetch = require('node-fetch');
+const { configAdmin } = require('./lib/config');
 
 const DIR_TEMP = './temp';
 const NAME_TRACK_AUDIO = 'footage/TodoONada_Final_Audio.m4a';
 const url = process.env.NEXT_PUBLIC_URL_SITE;
 
+// replace videos name
+// here get video according to character and save to disk
+
+const videosListCharacter = (character, videoId, videoListAll) => {
+  let allVideos = [];
+
+  videoListAll.forEach((video, index) => {
+    if (video.character === character || video.character === 'mix') {
+      // new name
+      const name = `${videoId}-${index}.mp4`;
+      console.log(name);
+
+      // get video
+      const getVideo = loadFileSync(path.join(DIR_TEMP, video.name));
+
+      // save video
+      const pathName = path.join(DIR_TEMP, name)
+      writeFileSync(pathName, getVideo);
+
+      allVideos.push(name);
+    }
+  });
+
+  // return new array videos
+  return allVideos;
+};
+
 export default async (req, res) => {
-  const { userId, character, faceId, locale } = JSON.parse(req.body);
+  const userId = cuid();
+  const locales = ['en', 'es'];
+  const locale = locales[locales.length * Math.random() | 0];
 
-  /********************************************************************************************************************
-   * SWAP VIDEOS PROCESS
-   * Each process here depends on the completion of the other. In the specific case of refase, 
-   * the faceId is required first to process the swaps, and it is required to have the swap videos 
-   * to go on to concat the entire final video.
-   ********************************************************************************************************************/
-  // 3. Swap videos and get ids
-  const videosListCharacter = character === 'female' ? videosListFemale(faceId) : videosListMale(faceId);
-  //console.log('Videos List Character', videosListCharacter);
+  // generate male or female ramdom
+  const characters = ['female', 'male'];
+  const character = characters[characters.length * Math.random() | 0];
 
-
+  try {
+    
+    /********************************************************************************************************************
+     * SWAP VIDEOS PROCESS
+     * Each process here depends on the completion of the other. In the specific case of refase, 
+     * the faceId is required first to process the swaps, and it is required to have the swap videos 
+     * to go on to concat the entire final video.
+     ********************************************************************************************************************/
+    // Step 3. Swap videos and get ids
+    // Here we do not swap, but if we call the videos according to character and save them to disk to use
+    const adjustVideos = await videosListCharacter(character, userId, videoListAll);
+    console.log('Adjust videos', adjustVideos);
+    
     // 5. write file .txt with info videos
     const nameFileVideos = `videos-${userId}.txt`;
     writeFileSync( path.join(DIR_TEMP, nameFileVideos), buildFileVideos(adjustVideos, videoListAll, character) );
@@ -44,19 +83,70 @@ export default async (req, res) => {
       track: path.join(DIR_TEMP, NAME_TRACK_AUDIO),
     }
     console.log('track video and audio', dataTrack);
-
+  
     await changeTrackFluent(dataTrack);
-
+  
+    let removeSubVideos = [];
+    const allSubVideos = adjustVideos.map((video, index) => {
+      const pathFile = path.join(DIR_TEMP, video);
+      removeSubVideos[index] = pathFile;
+    });
+  
+    removeFileSync(dataFinal.output);
+    removeFileSync(dataTrack.output);
+    removeFileSync(removeSubVideos);
+    removeFileSync(dataFinal.fileVideos);
+  
     const pathLocale = locale === 'es' ? '/es/' : '/';
+  
     const data = {
       userId,
       urlShare: `${url}${pathLocale}share-experience/${userId}`, 
       urlJoin: `${url}${pathLocale}join-experience/${userId}`,
-      urlPhoto: footage[0],
-      urlVideo: footage[1],
-      footage,
+      // urlPhoto: footage[0],
+      // urlVideo: footage[1],
+      // footage,
+    };
+  
+    const dataAdmin = {
+      participant_id: userId,
+      firstname: `test ${userId}`,
+      lastname: `test ${userId}`,
+      email: `test-${userId}@email.com`,
+      zipcode: '01234',
+      character,
+      url_photo: `urlPhoto/${userId}`,
+      url_video: `urlVideo/${userId}`,
+      productnews: 1,
+      testdrive: 1,
+      footage: data,
     };
     
-    res.status(200).send({ success: true, data });
+    let jsonAdmin;
 
+    try {
+      const resAdmin = await fetch(`${configAdmin.url_api}/participant`, {
+        method: 'POST',
+        body:    JSON.stringify(dataAdmin),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    
+      jsonAdmin = await resAdmin.json();
+      
+      if(!jsonAdmin.participant) {
+        // return
+        res.status(200).send({ success: false, action: 'saveParticipant', userExist: jsonAdmin.participant });
+      }
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ success: false, action: 'saveParticipant', error});
+    }
+
+    res.status(200).send({ success: true, dataAdmin });
+
+  } catch (error) {
+
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
